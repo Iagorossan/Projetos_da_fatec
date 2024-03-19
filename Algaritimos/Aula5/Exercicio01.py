7b27306b39ca8755c3a6466c6bc9ab9ec0500a19
IagoYuriRossan <iagorossan321@gmail.com>
2024-03-18 21:59:50 -0300

    Adicionando exercicios e exemplos da aula 05

---
+++ Algaritimos/Aula5/Exercicio01.py
@@ -0,0 +1,7 @@
+numero1= int(input("Digite um numero!"))
+numero2 = int(input("Digite um numero!"))
+
+if numero1 > numero2:
+    print(numero1)
+else:
+    print (numero2)
---
+++ Algaritimos/Aula5/Exercicio02.py
@@ -0,0 +1,9 @@
+fruta = (input("Digite uma fruta!")).upper()
+if fruta == 'Banana':
+    print("O quilo da Banana é 5,23")
+elif fruta == 'Maça':
+    print("O quilo da Maça é 12,10")
+elif fruta == 'Cereja':
+    print("O quilo da Cereja é 58,00")
+else:
+    print("Desculpe-nos. A fruta", fruta, "está em falta")
