2a985d316570070eb237b7d6f1524917b76f4d99
IagoYuriRossan <iagorossan321@gmail.com>
2024-03-25 21:06:20 -0300

    segunda leva da Lista de exercicios aula6

--- Algaritimos/Aula6/Exercicio3.py
+++ Algaritimos/Aula6/Exercicio3.py
@@ -1,5 +1,14 @@
-largura = float(input(print("Entre com a largura do quarto: "))
-comprimento = float(input(print("Entre com o comprimento do quarto")))
-area = largua * comprimento
-litros_necessários = area * 3
-print("São necessários", litros_necessários, "liros para pinar o quarto")
+largura_aposento = float(input("Digite a largura do aposento em metros: "))
+comprimento_aposento = float(input("Digite o comprimento do aposento em metros: "))
+
+altura_parede = 2.80
+largura_porta = 0.80
+altura_porta = 2.10
+
+area_paredes = 2 * altura_parede * (largura_aposento + comprimento_aposento) - (largura_porta * altura_porta)
+
+rendimento_tinta = 3
+
+latas_tinta = (area_paredes / rendimento_tinta)
+
+print("Você precisará de latas de tinta.",latas_tinta)
